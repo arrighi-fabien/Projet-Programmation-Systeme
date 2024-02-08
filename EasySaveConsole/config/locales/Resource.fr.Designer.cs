ba466377efe97916356445d69e052962cb0e94e9
Fabien Arrighi <fabien2a.arrighi@protonmail.com>
2024-02-08 01:31:42 +0100

    🔨 Change class name
    - Add a upper case letter to class names to suppress alerts

--- EasySaveConsole/config/locales/Resource.fr.Designer.cs
+++ EasySaveConsole/config/locales/Resource.fr.Designer.cs
@@ -22,14 +22,14 @@ namespace EasySaveConsole {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class fr {
+    internal class Fr {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal fr() {
+        internal Fr() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace EasySaveConsole {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EasySaveConsole.config.locales.fr", typeof(fr).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EasySaveConsole.config.locales.Fr", typeof(Fr).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
