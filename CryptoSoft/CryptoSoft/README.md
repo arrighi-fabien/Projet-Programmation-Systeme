9dfd3523c4a2ac46e47c01c15d1f647884bfd617
Fabien Arrighi <fabien2a.arrighi@protonmail.com>
2024-02-16 11:35:40 +0100

    🚀 Base for v2.0

---
+++ CryptoSoft/CryptoSoft/README.md
@@ -0,0 +1,21 @@
+﻿# CryptoSoft By Prosoft
+
+[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)  [![forthebadge](http://forthebadge.com/images/badges/powered-by-electricity.svg)](http://forthebadge.com)
+
+This project is a simple console application to encrypt file with XOR algorithm.
+
+## Usage
+
+```bash
+$ cryptosoft -h
+
+Usage of cryptosoft:
+  -d string
+		Decrypt file
+  -e string
+		Encrypt file
+  -k string
+		Key to encrypt/decrypt
+  -o string
+		Output file
+```
---
+++ EasySaveGUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace EasySaveGUI {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e) {
+            OpenNewWindow(new SettingsWindow());
+        }
+
+        private void SaveJobButton_Click(object sender, RoutedEventArgs e) {
+            OpenNewWindow(new SaveJobWindow());
+        }
+
+        private void OpenNewWindow(Window window) {
+            this.IsEnabled = false;
+            window.Owner = this;
+            window.Closed += (s, args) => { this.IsEnabled = true; };
+            window.Show();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            foreach (Window window in Application.Current.Windows) {
+                if (window != this && window.IsVisible) {
+                    e.Cancel = true;
+                    MessageBox.Show("Please close all windows before exiting the application.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+        }
+
+        private void DeleteSaveJobButton_Click(object sender, RoutedEventArgs e) {
+            // If no save job is selected, show an error message
+            if (this.SaveJobList.Items.Count == 0) {
+                MessageBox.Show("No save job selected.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
