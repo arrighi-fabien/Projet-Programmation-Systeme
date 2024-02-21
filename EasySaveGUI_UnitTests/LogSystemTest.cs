a3fd8543395200cfdf8e2c2a4d002743f935787a
Enzo <marcelli.enzo@gmail.com>
2024-02-21 16:13:22 +0100

    All unit tests except Encrypt and wpf files

---
+++ EasySaveGUI_UnitTests/LogSystemTest.cs
@@ -0,0 +1,29 @@
+﻿using EasySaveGUI.model;
+
+namespace EasySaveGUI.Tests {
+    [TestClass]
+    public class LogSystemTests {
+
+        [TestMethod]
+        public void LogSystem_ConstructsCorrectly() {
+            // Arrange
+            string name = "TestJob";
+            string sourceFile = "C:\\Source\\test.txt";
+            string destinationFile = "D:\\Destination\\test.txt";
+
+            // Act
+            LogSystem logSystem = new TestLogSystem(name, sourceFile, destinationFile);
+
+            // Assert
+            Assert.AreEqual(name, logSystem.Name);
+            Assert.AreEqual(sourceFile, logSystem.SourceFile);
+            Assert.AreEqual(destinationFile, logSystem.DestinationFile);
+        }
+
+        // Mock class for testing LogSystem
+        public class TestLogSystem : LogSystem {
+            public TestLogSystem(string name, string sourceFile, string destinationFile) : base(name, sourceFile, destinationFile) {
+            }
+        }
+    }
+}
---
+++ EasySaveGUI_UnitTests/SaveJobTests.cs
@@ -0,0 +1,41 @@
+﻿using EasySaveGUI.model;
+using System.IO;
+
+namespace EasySaveGUI.Tests {
+    [TestClass]
+    public class SaveJobTests {
+        [TestMethod]
+        public void GetFileList_ReturnsCorrectFileCount() {
+            // Arrange
+            string testFolderPath = "C:\\TestFolder";
+            Directory.CreateDirectory(testFolderPath);
+            File.Create(Path.Combine(testFolderPath, "testfile1.txt")).Close();
+            File.Create(Path.Combine(testFolderPath, "testfile2.txt")).Close();
+            File.Create(Path.Combine(testFolderPath, "testfile3.txt")).Close();
+            Directory.CreateDirectory(Path.Combine(testFolderPath, "Subfolder"));
+            File.Create(Path.Combine(testFolderPath, "Subfolder", "testfile4.txt")).Close();
+
+            SaveJob saveJob = new MockSaveJob();
+
+            // Act
+            List<string> files = new List<string>();
+            saveJob.GetFileList(testFolderPath, files);
+
+            // Assert
+            // Expected count should be 4 because we have 3 files and 1 subfolder
+            Assert.AreEqual(4, files.Count);
+
+            // Clean up
+            Directory.Delete(testFolderPath, true);
+        }
+
+        // Mock class to test abstract methods of SaveJob
+        public class MockSaveJob : SaveJob {
+            public MockSaveJob() : base("", "", "") { }
+
+            public override bool IsToSave(string path) {
+                return true;
+            }
+        }
+    }
+}
