42c471c75bc1bc16f4d9071657f6494b825667c9
Fabien Arrighi <fabien2a.arrighi@protonmail.com>
2024-02-27 12:19:10 +0100

    🚀 Prohibit parallel transfer of files > n Ko
    - Max size can be change in settings

--- EasySaveGUI/SettingsWindow.xaml.cs
+++ EasySaveGUI/SettingsWindow.xaml.cs
@@ -44,6 +44,10 @@ namespace EasySaveGUI {
             EncryptExtensionTextBox.Text = savedEncryptExtensions.Replace(";", "\r\n");
             string savedProfessionalApp = tool.GetConfigValue("professsionalApp");
             ProfessionalAppTextBox.Text = savedProfessionalApp.Replace(";", "\r\n");
+            string savedPriorityExtensions = tool.GetConfigValue("priorityExtensions");
+            PriorityExtensionTextBox.Text = savedPriorityExtensions.Replace(";", "\r\n");
+            string savedFileSize = tool.GetConfigValue("fileSize");
+            FileSizeTextBox.Text = savedFileSize;
 
             Refresh();
         }
@@ -86,6 +90,9 @@ namespace EasySaveGUI {
             SaveTextAreas(PriorityExtensionTextBox.Text, "priorityExtensions");
             SaveTextAreas(EncryptExtensionTextBox.Text, "encryptExtensions");
             SaveTextAreas(ProfessionalAppTextBox.Text, "professsionalApp");
+            // Convert value to int or set to 10 if not a number
+            int fileSize = int.TryParse(FileSizeTextBox.Text, out int result) ? result : 10;
+            tool.WriteConfigValue("fileSize", fileSize.ToString());
         }
 
         private void SaveTextAreas(string content, string key) {
