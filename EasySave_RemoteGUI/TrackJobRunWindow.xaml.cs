7a11d0f1afd4fda1de7127ca79e5253d2becaa86
Fabien Arrighi <fabien2a.arrighi@protonmail.com>
2024-02-29 14:32:13 +0100

    🤡 Add missing file

--- EasySave_RemoteGUI/TrackJobRunWindow.xaml.cs
+++ EasySave_RemoteGUI/TrackJobRunWindow.xaml.cs
@@ -1,9 +1,9 @@
-﻿using EasySave_RemoteGUI.model;
-using System.IO;
+﻿using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
+using EasySave_RemoteGUI.model;
 
 namespace EasySave_RemoteGUI {
     /// <summary>
@@ -14,6 +14,7 @@ namespace EasySave_RemoteGUI {
 
         private bool FirstTime = true;
         private List<JobState> jobStates = new();
+        private List<JobState> newJobStates = new();
 
         // Constructor that accepts a TcpClient
         public TrackJobRunWindow(TcpClient client) {
@@ -27,14 +28,23 @@ namespace EasySave_RemoteGUI {
         // Method to listen for messages from the server
         private async void ListenForMessages() {
             try {
+                string beforeData = string.Empty;
+                string afterData = string.Empty;
                 while (client != null && client.Connected) {
                     NetworkStream stream = client.GetStream();
                     byte[] buffer = new byte[1024];
                     int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
                     if (bytesRead > 0) {
                         string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        // Remove all content before the first <data> tag
+                        message = message.Substring(message.IndexOf("<data>") + 6);
+                        // Remove all content after the first </data> tag after the first <data> tag
+                        message = message.Substring(0, message.IndexOf("</data>"));
+                        if (message.Length < 0) {
+                            continue;
+                        }
                         Dispatcher.Invoke(() => {
-                            jobStates = JsonSerializer.Deserialize<List<JobState>>(message);
+                            newJobStates = JsonSerializer.Deserialize<List<JobState>>(message);
                             UpdateUI();
                         });
                     }
@@ -59,17 +69,19 @@ namespace EasySave_RemoteGUI {
 
         // Method to update the UI with job state data
         private void UpdateUI() {
-
-/*            if (FirstTime) {
+            if (FirstTime) {
+                jobStates = newJobStates;
                 JobStateListBox.ItemsSource = jobStates;
                 FirstTime = false;
-            }*/
-            JobStateListBox.Items.Clear();
-            JobStateListBox.ItemsSource = jobStates;
-
-            //foreach (var jobState in jobStates) {
-            //JobStateListBox.Items.Add($"Name: {jobState.Name}, Progress: {jobState.Progression}%");
-            //}
+            }
+            else {
+                for (int i = 0; i < jobStates.Count; i++) {
+                    jobStates[i].Name = newJobStates[i].Name;
+                    jobStates[i].State = newJobStates[i].State;
+                    jobStates[i].Progression = newJobStates[i].Progression;
+                    JobStateListBox.Items.Refresh();
+                }
+            }
         }
     }
 }
